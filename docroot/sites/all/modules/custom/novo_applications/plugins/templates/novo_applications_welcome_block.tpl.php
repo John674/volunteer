6917658ec83719796c45ac6ce65c89df00ecb509
Sergey Grigorenko <svipsa@gmail.com>
2017-06-15 15:56:14 +0300

    Added panels and differetn frontpage for volunteer and admin

---
+++ docroot/sites/all/modules/custom/novo_applications/plugins/templates/novo_applications_welcome_block.tpl.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * Default theme Frontpage welcome block.
+ */
+?>
+<div class="container">
+
+  <?php
+   $already_created_app = isset($data['#content']['already_created_app']) ? $data['#content']['already_created_app'] : FALSE;
+  ?>
+
+
+  <?php if ($already_created_app) : ?>
+    <h2>ALREADY CREATED</h2>
+    <?php endif; ?>
+
+  Welcome, Sergey(Test) Ivanov!
+  Your application will begin processing now! We will keep you updated on our progress by email, and please feel free to contact us at anytime for more information (volunteer@novoministries.org or 405.208.4255).
+
+  We generally complete volunteer applications in 3 - 7 days.
+
+  We’re so excited to work together to introduce the boys and girls of Oklahoma City to Jesus Christ. Thank you!
+
+  Made a mistake on your application?
+</div>
