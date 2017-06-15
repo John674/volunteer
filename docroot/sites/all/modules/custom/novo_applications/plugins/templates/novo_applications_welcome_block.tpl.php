a56c08a99bd98db7ff87cb405356ac98cd45d413
Sergey Grigorenko <svipsa@gmail.com>
2017-06-15 16:22:09 +0300

    CS

--- docroot/sites/all/modules/custom/novo_applications/plugins/templates/novo_applications_welcome_block.tpl.php
+++ docroot/sites/all/modules/custom/novo_applications/plugins/templates/novo_applications_welcome_block.tpl.php
@@ -8,20 +8,23 @@
 <div class="container">
 
   <?php
-   $already_created_app = isset($data['#content']['already_created_app']) ? $data['#content']['already_created_app'] : FALSE;
+  $already_created_app = isset($data['#content']['already_created_app']) ? $data['#content']['already_created_app'] : FALSE;
   ?>
 
 
   <?php if ($already_created_app) : ?>
-    <h2>ALREADY CREATED</h2>
-    <?php endif; ?>
+      <h2>ALREADY CREATED</h2>
+  <?php endif; ?>
 
-  Welcome, Sergey(Test) Ivanov!
-  Your application will begin processing now! We will keep you updated on our progress by email, and please feel free to contact us at anytime for more information (volunteer@novoministries.org or 405.208.4255).
+    Welcome, Sergey(Test) Ivanov!
+    Your application will begin processing now! We will keep you updated on our
+    progress by email, and please feel free to contact us at anytime for more
+    information (volunteer@novoministries.org or 405.208.4255).
 
-  We generally complete volunteer applications in 3 - 7 days.
+    We generally complete volunteer applications in 3 - 7 days.
 
-  We’re so excited to work together to introduce the boys and girls of Oklahoma City to Jesus Christ. Thank you!
+    We’re so excited to work together to introduce the boys and girls of
+    Oklahoma City to Jesus Christ. Thank you!
 
-  Made a mistake on your application?
+    Made a mistake on your application?
 </div>
--- docroot/sites/all/themes/custom/novo/templates/fields/applications/field--field_goes_by--application.tpl.php
+++ docroot/sites/all/themes/custom/novo/templates/fields/applications/field--field_goes_by--application.tpl.php
@@ -1,8 +1,7 @@
 <?php
 
 /**
- * @file field.tpl.php
- * Default template implementation to display the value of a field.
+ * @file Default template implementation to display the value of a field.
  *
  * This file is not used and is here as a starting point for customization only.
  * @see theme_field()
