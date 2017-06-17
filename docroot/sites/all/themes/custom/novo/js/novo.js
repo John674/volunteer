405e3ba688e445e1fea1a9023906775c736e6575
Sergey Grigorenko <svipsa@gmail.com>
2017-06-17 18:58:14 +0300

    Themed program form

--- docroot/sites/all/themes/custom/novo/js/novo.js
+++ docroot/sites/all/themes/custom/novo/js/novo.js
@@ -5,6 +5,12 @@
     Drupal.behaviors.Novo = {
         attach: function (context, settings) {
             $('.selectpicker').selectpicker('refresh');
+
+            // $(".field-name-field-program-staff-list select", context).selectpicker({
+            //     liveSearch: true,
+            //     selectedTextFormat: 'count > 3'
+            // });
+
         }
     };
 })(jQuery);
