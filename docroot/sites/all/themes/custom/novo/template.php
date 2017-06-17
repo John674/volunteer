1bcaefdab4294aa4db405a189d31d7777d7955e8
Sergey Grigorenko <svipsa@gmail.com>
2017-06-17 20:24:11 +0300

    Fixes

--- docroot/sites/all/themes/custom/novo/template.php
+++ docroot/sites/all/themes/custom/novo/template.php
@@ -56,7 +56,7 @@ function novo_preprocess_form(&$variables) {
 function novo_preprocess_field(&$variables) {
   $node = $variables['element']['#object'];
 
-  if (isset($node->type) && $node->type == 'application' || $node->type == 'kids') {
+  if (isset($node->type) && ($node->type == 'application' || $node->type == 'kids')) {
 
     switch ($variables['element']['#field_name']) {
       case 'field_masked_phone_1':
