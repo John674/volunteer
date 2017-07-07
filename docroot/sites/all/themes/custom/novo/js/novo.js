7067fc700988fc55173c9a7e73fc447da765a105
Vasiliy <vvasiiy29@gmail.com>
2017-07-07 15:27:24 +0300

    Change name Publish to Active.

--- docroot/sites/all/themes/custom/novo/js/novo.js
+++ docroot/sites/all/themes/custom/novo/js/novo.js
@@ -5,12 +5,20 @@
     Drupal.behaviors.Novo = {
         attach: function (context, settings) {
             $('.selectpicker').selectpicker('refresh');
+            ChangeNamePublishToActive();
 
             // $(".field-name-field-program-staff-list select", context).selectpicker({
             //     liveSearch: true,
             //     selectedTextFormat: 'count > 3'
             // });
-
+            function ChangeNamePublishToActive() {
+                var $publish_btn = $('#edit-publish');
+                if ($publish_btn.val() == 'Publish') {
+                    $publish_btn.text('Active');
+                } else {
+                    $publish_btn.text('Not active');
+                }
+            }
         }
     };
-})(jQuery);
+})(window.jQuery);
