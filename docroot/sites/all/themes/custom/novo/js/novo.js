9f94a1eb7b56dcead4e7ac2cff4f452d707c5ccf
Sergey Grigorenko <svipsa@gmail.com>
2017-07-28 14:58:55 +0300

    Validate DOB

--- docroot/sites/all/themes/custom/novo/js/novo.js
+++ docroot/sites/all/themes/custom/novo/js/novo.js
@@ -12,6 +12,12 @@
 
             $("button.btn-success:not(.icon-before)").addClass("icon-before").prepend('<span class="icon glyphicon glyphicon-ok" aria-hidden="true"></span>');
 
+            if ($(".field-name-field-dob input", context).length) {
+                $(".field-name-field-dob input", context).datepicker({
+                    maxDate: new Date(),
+                });
+            }
+
         }
     };
 })(window.jQuery);
