17f4de78b79dce50d04d48d86b38e35ce769ca45
Sergey Grigorenko <svipsa@gmail.com>
2017-09-01 16:54:27 +0300

    Report Active Children #273

--- docroot/sites/all/themes/custom/novo/templates/exports/views-data-export-pdf-header.tpl.php
+++ docroot/sites/all/themes/custom/novo/templates/exports/views-data-export-pdf-header.tpl.php
@@ -17,6 +17,18 @@
   <?php print (isset($head) && !empty($head)) ? $head : ""; ?>
 </head>
 <body>
+
+<?php
+if (!$logo = theme_get_setting('logo_path')) {
+  $logo = theme_get_setting('logo');
+}
+?>
+<?php if ($logo): ?>
+    <div style="background: #626463; margin: 20px; 0px; padding: 10px; width:100%;">
+      <?php print theme('image', array('path' => $logo)); ?>
+    </div>
+<?php endif; ?>
+
 <table border="0" cellpadding="5" cellspacing="0">
   <?php print $header_row; ?>
     <tbody>
