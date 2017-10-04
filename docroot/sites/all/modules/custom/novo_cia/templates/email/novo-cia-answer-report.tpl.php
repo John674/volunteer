5335304234047f0dabf656a7cc553ba44c805d48
Vasiliy <vvasiiy29@gmail.com>
2017-10-04 16:49:45 +0300

    Cia Integration.

---
+++ docroot/sites/all/modules/custom/novo_cia/templates/email/novo-cia-answer-report.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file
+ * CIA email theme.
+ */
+?>
+
+<p>Hello <?php print $params['reviewer_name'];?>,</p>
+<p>CIA Answer is <?php print $params['cia_answer']; ?> for <?php print $params['vol_name']; ?>, please open Novo Volunteer <a target="_blank" href="<?php print $params['vol_link'] ?>">system</a>.</p>
+<p>Thanks.</p>
