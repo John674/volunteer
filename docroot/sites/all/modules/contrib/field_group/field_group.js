d2fcfc9c0e6588e55a86b0f3c1442e2e9a4c0ef3
Sergey Grigorenko <svipsa@gmail.com>
2017-11-21 19:39:23 +0300

    Fixes

--- docroot/sites/all/modules/contrib/field_group/field_group.js
+++ docroot/sites/all/modules/contrib/field_group/field_group.js
@@ -69,14 +69,14 @@ Drupal.FieldGroup.Effects.processAccordion = {
 
         // Add required fields mark to any element containing required fields
         wrapper.find('div.field-group-accordion-item').each(function(i) {
-
           if ($(this).is('.required-fields') && $(this).find('.form-required').length > 0) {
             $('h3.ui-accordion-header a').eq(i).append(' ').append($('.form-required').eq(0).clone());
           }
           if ($('.error', $(this)).length) {
             // Save first error item, for focussing it.
             if (!$firstErrorItem) {
-              $firstErrorItem = $(this).parent().accordion('option', "activate" , i);
+              $firstErrorItem = $(this).parent().accordion('option', "active" , i);
+
             }
             $('h3.ui-accordion-header').eq(i).addClass('error');
           }
